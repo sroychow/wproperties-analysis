e1a6cc83d7d61da114f9737fb842cc3ecb2379f2
Elisabetta Manca <elisabetta.manca@cern.ch>
2021-01-10 17:46:50 +0100

    merge with external filtering modules

--- templateMaker/data/binning.py
+++ templateMaker/data/binning.py
@@ -4,3 +4,6 @@ qtBins = ROOT.vector('float')([0., 4., 8., 12., 16., 20., 24., 28., 32., 40., 60
 yBins = ROOT.vector('float')([0, 0.4, 0.8, 1.2, 1.6, 2.0, 2.4, 3.0, 6.0])
 ptBins = ROOT.vector('float')([25.+i*0.5 for i in range(61)])
 etaBins = ROOT.vector('float')([-2.4+i*0.1 for i in range(49)])
+chargeBins = ROOT.vector('float')(-2. +i*2. for i in range(3))
+mTBins = ROOT.vector('float')([0.,30.,40.,2000.])
+isoBins = ROOT.vector('float')([0.,0.15,1.])
--- templateMaker/runSignal.py
+++ templateMaker/runSignal.py
@@ -9,10 +9,6 @@ import matplotlib.pyplot as plt
 import mplhep as hep
 from root_numpy import array2hist
 from array import array
-
-plt.style.use([hep.style.ROOT, hep.style.firamath])
-#hep.cms.label(loc=0)
-hep.cms.text('Simulation')
 sys.path.append('../RDFprocessor/framework')
 from RDFtree import RDFtree
 sys.path.append('python/')
@@ -20,6 +16,11 @@ from getLumiWeight import getLumiWeight
 sys.path.append('data/')
 from binning import qtBins, yBins, ptBins, etaBins
 from externals import filePt, fileY, fileSF
+
+# matplotlib stuff
+plt.style.use([hep.style.ROOT, hep.style.firamath])
+#hep.cms.label(loc=0)
+hep.cms.text('Simulation')
 
 ROOT.gSystem.Load('bin/libAnalysisOnData.so')
 ROOT.gROOT.ProcessLine("gErrorIgnoreLevel = 2001;")
@@ -29,43 +30,43 @@ ROOT.ROOT.EnableImplicitMT(64)
 outputDir = 'PLOTS'
 inputFile = '/scratchnvme/wmass/WJetsNoCUT_v2/tree_*_*.root'
 
-# p = RDFtree(outputDir = outputDir, inputFile = inputFile, outputFile="test.root", pretend=False)
-# p.branch(nodeToStart='input', nodeToEnd='defs', modules=[getLumiWeight(xsec=61526.7, inputFile=inputFile),ROOT.reweightFromZ(filePt,fileY), ROOT.genDefinitions(),ROOT.defineHarmonics(), ROOT.Replica2Hessian()])
-# harmonics = {"P0" : (20./3., 1./10),"P1": (5.,0.), "P2": (20.,0.), "P3": (4.,0.),"P4":(4.,0.),"P5":(5.,0.),"P6":(5.,0.),"P7":(4.,0.)}
+p = RDFtree(outputDir = outputDir, inputFile = inputFile, outputFile="test.root", pretend=False)
+p.branch(nodeToStart='input', nodeToEnd='defs', modules=[getLumiWeight(xsec=61526.7, inputFile=inputFile),ROOT.reweightFromZ(filePt,fileY), ROOT.genDefinitions(),ROOT.defineHarmonics(), ROOT.Replica2Hessian()])
+harmonics = {"P0" : (20./3., 1./10),"P1": (5.,0.), "P2": (20.,0.), "P3": (4.,0.),"P4":(4.,0.),"P5":(5.,0.),"P6":(5.,0.),"P7":(4.,0.)}
 
-# for harm in harmonics:
-#     if "P5" in harm or "P6" in harm or "P7" in harm: # don't make variations
-#         p.Histogram(columns = ["Wrap_preFSR_abs","Wpt_preFSR","{}".format(harm),"lumiweight", "weightPt"], types = ['float']*5,node='defs',histoname=ROOT.string("xsecs_{}".format(harm)),bins = [yBins,qtBins])
-#     else:
-#         p.Histogram(columns = ["Wrap_preFSR_abs","Wpt_preFSR","{}".format(harm),"lumiweight", "weightPt"], types = ['float']*5,node='defs',histoname=ROOT.string("xsecs_{}".format(harm)),bins = [yBins,qtBins])
-# p.Histogram(columns = ["Wrap_preFSR_abs","Wpt_preFSR","lumiweight", "weightPt"], types = ['float']*4,node='defs',histoname=ROOT.string("xsecs"),bins = [yBins,qtBins])
-# p.gethdf5Output()
+for harm in harmonics:
+    if "P5" in harm or "P6" in harm or "P7" in harm: # don't make variations
+        p.Histogram(columns = ["Wrap_preFSR_abs","Wpt_preFSR","{}".format(harm),"lumiweight", "weightPt"], types = ['float']*5,node='defs',histoname=ROOT.string("xsecs_{}".format(harm)),bins = [yBins,qtBins])
+    else:
+        p.Histogram(columns = ["Wrap_preFSR_abs","Wpt_preFSR","{}".format(harm),"lumiweight", "weightPt"], types = ['float']*5,node='defs',histoname=ROOT.string("xsecs_{}".format(harm)),bins = [yBins,qtBins])
+p.Histogram(columns = ["Wrap_preFSR_abs","Wpt_preFSR","lumiweight", "weightPt"], types = ['float']*4,node='defs',histoname=ROOT.string("xsecs"),bins = [yBins,qtBins])
+p.gethdf5Output()
 
-# shape = (len(yBins)-1) * (len(qtBins)-1) 
+shape = (len(yBins)-1) * (len(qtBins)-1) 
 
-# fewk = h5py.File('PLOTS/test.hdf5', mode='r+')
-# htot = np.array(fewk['xsecs'][:].reshape((len(yBins)-1,len(qtBins)-1),order='F'),order='C')
+fewk = h5py.File('PLOTS/test.hdf5', mode='r+')
+htot = np.array(fewk['xsecs'][:].reshape((len(yBins)-1,len(qtBins)-1),order='F'),order='C')
 
-# f = ROOT.TFile('PLOTS/angcoeff.root','recreate')
-# for harmonic, factors in harmonics.items():
-#     hewk = np.array(fewk['xsecs_{}'.format(harmonic)][:].reshape((len(yBins)-1,len(qtBins)-1),order='F'),order='C')
-#     hep.hist2dplot(factors[0]*(hewk/htot+factors[1]), yBins, qtBins)
-#     plt.savefig('PLOTS/xsecs_{}.png'.format(harmonic))
-#     plt.clf()
-#     #ROOT format to feed RDF
-#     th2 = ROOT.TH2D('xsecs_{}'.format(harmonic),'xsecs_{}'.format(harmonic), len(yBins)-1, array('d',yBins), len(qtBins)-1, array('d',qtBins))
-#     hist = array2hist(factors[0]*(hewk/htot+factors[1]), th2, errors=None)
-#     f.cd()
-#     hist.Write()
+f = ROOT.TFile('PLOTS/angcoeff.root','recreate')
+for harmonic, factors in harmonics.items():
+    hewk = np.array(fewk['xsecs_{}'.format(harmonic)][:].reshape((len(yBins)-1,len(qtBins)-1),order='F'),order='C')
+    hep.hist2dplot(factors[0]*(hewk/htot+factors[1]), yBins, qtBins)
+    plt.savefig('PLOTS/xsecs_{}.png'.format(harmonic))
+    plt.clf()
+    #ROOT format to feed RDF
+    th2 = ROOT.TH2D('xsecs_{}'.format(harmonic),'xsecs_{}'.format(harmonic), len(yBins)-1, array('d',yBins), len(qtBins)-1, array('d',qtBins))
+    hist = array2hist(factors[0]*(hewk/htot+factors[1]), th2, errors=None)
+    f.cd()
+    hist.Write()
 
-# hep.hist2dplot(htot, yBins, qtBins)
-# plt.savefig('PLOTS/xsecs_{}.png'.format(harmonic))
-# plt.clf()
-# th2 = ROOT.TH2D('xsecs','xsecs', len(yBins)-1, array('d',yBins), len(qtBins)-1, array('d',qtBins))
-# hist = array2hist(htot, th2, errors=None)
-# f.cd()
-# hist.Write()
-# f.Close()
+hep.hist2dplot(htot, yBins, qtBins)
+plt.savefig('PLOTS/xsecs_{}.png'.format(harmonic))
+plt.clf()
+th2 = ROOT.TH2D('xsecs','xsecs', len(yBins)-1, array('d',yBins), len(qtBins)-1, array('d',qtBins))
+hist = array2hist(htot, th2, errors=None)
+f.cd()
+hist.Write()
+f.Close()
 
 f = ROOT.TFile.Open('PLOTS/angcoeff.root')
 
