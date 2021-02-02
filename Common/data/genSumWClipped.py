0a10686ab75a22f3b7bb2e6cfa65ae8df9e85326
Suvankar Roy Chowdhury <suvankar.roy.chowdhury@cern.ch>
2021-02-02 12:18:32 +0100

    adding script to compute genEventSum and updating the dict

--- Common/data/genSumWClipped.py
+++ Common/data/genSumWClipped.py
@@ -1,6 +1,18 @@
+#using genWeight as the branch
 sumwClippedDict = {
-    "DYJetsToMuMu_M50": 236002045557.625, 
-    "DYJetsToTauTau_M50": 117743596494.25, 
+    "DYJetsToMuMu_M50": 235996573447.5537,
+    "DYJetsToTauTau_M50": 117740866311.92139,
+    "WPlusJetsToMuNu": 5895489808249.853,
+    "WMinusJetsToMuNu": 2001402133898.8691,
+    "WPlusJetsToTauNu": 805332767237.3389,
+    "WMinusJetsToTauNu": 357281504749.76074
+}
+
+'''
+#using Generator weight
+sumwClippedDict = {
+    "DYJetsToMuMu_M50":  236002045557.625, #Marco 2.36619888176e+11,  #236002045557.625, 
+    "DYJetsToTauTau_M50": 117743596494.25, #Marco 1.17935409707e+11, #117743596494.25, 
     "WPlusJetsToMuNu": 5895447715506.5, 
     "WMinusJetsToMuNu": 2001441443604.5, 
     "WPlusJetsToTauNu": 805327017069.5, 
@@ -12,3 +24,5 @@ sumwClippedDict = {
     "ST_t-channel_muDecays": 254958.0, 
     "ST_s-channel_4f_leptonDecays": 3687399.50390625
 }
+'''
+
---
+++ templateMaker/python/getGenWeightClipped.py
@@ -0,0 +1,70 @@
+import ROOT
+import json
+import sys
+import os
+
+sys.path.append('../../Common/data')
+
+ROOT.gInterpreter.Declare("""
+#include "ROOT/RDataFrame.hxx"
+#include "ROOT/RDF/RInterface.hxx"
+
+using RNode = ROOT::RDF::RNode;
+
+ROOT::RDF::RResultPtr<double> getClippedSumW(std::vector<std::string> fileNames)
+{
+  auto df = RNode(ROOT::RDataFrame("Events", fileNames));
+
+  auto clipGenWeight = [](float Gen_weight) {
+    double sign = Gen_weight / abs(Gen_weight);
+    //return sign;                                                                                                                                                                                   
+    double new_weight = double(std::min(fabs(Gen_weight), float(50000.)));
+    return sign * new_weight;
+  };
+  //auto d1 = df.Define("Generator_weight_clipped", clipGenWeight, {"Generator_weight"});                                                                                                            
+  auto d1 = df.Define("Generator_weight_clipped", clipGenWeight, {"genWeight"}).Sum<double>("Generator_weight_clipped");
+  return d1;
+}
+""")
+
+from samples_2016_ul import samplespreVFP
+
+def genwtsum(sample, fvec) :
+    print("processing ", sample)
+    
+ROOT.ROOT.EnableImplicitMT(64)
+
+inDir='/scratchnvme/wmass/NanoAOD2016-UL/preVFP/'
+samples = samplespreVFP
+sumwProc={}
+sumwRunProc = {}
+objList = []
+for sample in samples:
+    if 'data' in sample: continue
+    computeYes= 'DY' in sample or 'WPlus' in sample or 'WMinus' in sample
+    if not computeYes : continue
+    fvec=ROOT.vector('string')()
+    direc = samples[sample]['dir']
+    for d in direc:
+        targetDir='{}/{}/merged/'.format(inDir, d)
+        for f in os.listdir(targetDir):
+            if not f.endswith('.root'): continue
+            inputFile=targetDir+f
+            fvec.push_back(inputFile)
+    if fvec.empty():
+        print("No files found for directory:", samples[sample], " SKIPPING processing")
+        continue
+    print(fvec)  
+    sumwProc[sample] = ROOT.getClippedSumW(fvec)
+    objList.append(sumwProc[sample])
+
+ROOT.RDF.RunGraphs(objList)
+
+sumwClipped = {} 
+
+for sample in samples:
+    if sample not in sumwProc.keys() : continue
+    sumwClipped[sample]=sumwProc[sample].GetValue()
+
+with open('genSumWOutput.py', 'w') as fp:
+    json.dump(sumwClipped, fp, indent=4)
