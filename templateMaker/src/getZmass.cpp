d62d2e8b2efe0c506d2b0a830819a66d57c6cc04
Suvankar Roy Chowdhury <suvankar.roy.chowdhury@cern.ch>
2021-02-05 12:46:44 +0100

    fix to pt defn

--- templateMaker/src/getZmass.cpp
+++ templateMaker/src/getZmass.cpp
@@ -4,37 +4,14 @@
 
 RNode getZmass::run(RNode d)
 {
-  auto getdimuonMass = [](ROOT::VecOps::RVec<float> mu_pt, ROOT::VecOps::RVec<float> mu_eta,
+  auto getdimuonP4 = [](ROOT::VecOps::RVec<float> mu_pt, ROOT::VecOps::RVec<float> mu_eta,
                           ROOT::VecOps::RVec<float> mu_phi, ROOT::VecOps::RVec<float> mu_mass,
                           int mu1idx, int mu2idx) {
     TLorentzVector mu1P;
     mu1P.SetPtEtaPhiM(mu_pt[mu1idx], mu_eta[mu1idx], mu_phi[mu1idx], mu_mass[mu1idx]);
     TLorentzVector mu2P;
     mu2P.SetPtEtaPhiM(mu_pt[mu2idx], mu_eta[mu2idx], mu_phi[mu2idx], mu_mass[mu2idx]);
-    float mass = (mu1P + mu2P).M();
-    return mass;
-  };
-
-  auto getdimuonPt = [](ROOT::VecOps::RVec<float> mu_pt, ROOT::VecOps::RVec<float> mu_eta,
-                        ROOT::VecOps::RVec<float> mu_phi, ROOT::VecOps::RVec<float> mu_mass,
-                        int mu1idx, int mu2idx) {
-    TLorentzVector mu1P;
-    mu1P.SetPtEtaPhiM(mu_pt[mu1idx], mu_eta[mu1idx], mu_phi[mu1idx], mu_mass[mu1idx]);
-    TLorentzVector mu2P;
-    mu2P.SetPtEtaPhiM(mu_pt[mu2idx], mu_eta[mu2idx], mu_phi[mu2idx], mu_mass[mu2idx]);
-    float pt = (mu1P + mu2P).Pt();
-    return pt;
-  };
-
-  auto getdimuonRapidity = [](ROOT::VecOps::RVec<float> mu_pt, ROOT::VecOps::RVec<float> mu_eta,
-                              ROOT::VecOps::RVec<float> mu_phi, ROOT::VecOps::RVec<float> mu_mass,
-                              int mu1idx, int mu2idx) {
-    TLorentzVector mu1P;
-    mu1P.SetPtEtaPhiM(mu_pt[mu1idx], mu_eta[mu1idx], mu_phi[mu1idx], mu_mass[mu1idx]);
-    TLorentzVector mu2P;
-    mu2P.SetPtEtaPhiM(mu_pt[mu2idx], mu_eta[mu2idx], mu_phi[mu2idx], mu_mass[mu2idx]);
-    float y = (mu1P + mu2P).Rapidity();
-    return y;
+    return (mu1P + mu2P);
   };
 
   // careful: this requires opposite charge cut is applied!
@@ -61,9 +38,10 @@ RNode getZmass::run(RNode d)
                 .Define("Mu2_sip3d", getFromIdx, {"Muon_sip3d", "Idx_mu2"})
                 .Define("Mu2_dxy", getFromIdx, {"Muon_dxy", "Idx_mu2"})
                 .Define("Mu2_hasTriggerMatch", getIntFromIdx, {"Muon_hasTriggerMatch", "Idx_mu2"})
-                .Define("dimuonMass", getdimuonMass, {"Muon_pt", "Muon_eta", "Muon_phi", "Muon_mass", "Idx_mu1", "Idx_mu2"})
-                .Define("dimuonPt", getdimuonPt, {"Muon_pt", "Muon_eta", "Muon_phi", "Muon_mass", "Idx_mu1", "Idx_mu2"})
-                .Define("dimuonY", getdimuonRapidity, {"Muon_pt", "Muon_eta", "Muon_phi", "Muon_mass", "Idx_mu1", "Idx_mu2"})
+                .Define("dimuonP4", getdimuonP4, {"Muon_pt", "Muon_eta", "Muon_phi", "Muon_mass", "Idx_mu1", "Idx_mu2"})
+                .Define("dimuonMass", [](TLorentzVector p) { return float(p.M()); }, {"dimuonP4"})
+                .Define("dimuonPt", [](TLorentzVector p) { return float(p.Pt()); }, {"dimuonP4"})
+                .Define("dimuonY", [](TLorentzVector p) { return float(p.Rapidity()); }, {"dimuonP4"})
                 .Define("Mupos_pt", getByPosCharge, {"Mu1_pt", "Mu2_pt", "Mu1_charge"})
                 .Define("Muneg_pt", getByNegCharge, {"Mu1_pt", "Mu2_pt", "Mu1_charge"})
                 .Define("Mupos_eta", getByPosCharge, {"Mu1_eta", "Mu2_eta", "Mu1_charge"})
