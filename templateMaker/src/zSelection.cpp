61323d0b0c962adfee9121b6348bb6f976688bfe
Suvankar Roy Chowdhury <suvankar.roy.chowdhury@cern.ch>
2021-02-22 23:09:40 +0100

    use RVec logic in Zselection code

--- templateMaker/src/zSelection.cpp
+++ templateMaker/src/zSelection.cpp
@@ -12,19 +12,6 @@ RNode zSelection::run(RNode d) {
     return (mu1P + mu2P);
   };
 
-  //+ve muon is mu1
-  auto getprefmuonIdxFromCharge = [](RVec<int> mucharge) {
-    RVec<int> prefidx;
-    if(mucharge[0] > 0 && mucharge[1] < 0) {
-      prefidx.emplace_back(0);
-      prefidx.emplace_back(1);
-    } else if (mucharge[0] < 0 && mucharge[1] > 0) {
-      prefidx.emplace_back(1);
-      prefidx.emplace_back(0);
-    }
-    return prefidx;
-  };
-
   auto hasTriggerMatch = [](const float mueta, const float muphi, const RVec<float> &TrigObj_eta, const RVec<float> &TrigObj_phi) {
    bool muhasTrigm = false;
    for (unsigned int jtrig = 0; jtrig < TrigObj_eta.size(); ++jtrig) {
@@ -39,18 +26,17 @@ RNode zSelection::run(RNode d) {
 
   //.Define("Mu2_hasTriggerMatch", "Muon_hasTriggerMatch[prefIdx[1]]")
 
-  auto d1 = d.Define("prefIdx", getprefmuonIdxFromCharge, {"Muon_charge"})
-    .Define("Mu1_eta", "Muon_eta[prefIdx[0]]")
-    .Define("Mu1_phi", "Muon_phi[prefIdx[0]]")
-    .Define("Mu1_charge", "Muon_charge[prefIdx[0]]")
-    .Define("Mu1_relIso", "Muon_pfRelIso04_all[prefIdx[0]]")
-    .Define("Mu1_pt", "Muon_pt[prefIdx[0]]")
+  auto d1 = d.Define("Mu1_eta", "Muon_eta[Muon_charge>0][0]")
+    .Define("Mu1_phi", "Muon_phi[Muon_charge>0][0]")
+    .Define("Mu1_charge", "Muon_charge[Muon_charge>0][0]")
+    .Define("Mu1_relIso", "Muon_pfRelIso04_all[Muon_charge>0][0]")
+    .Define("Mu1_pt", "Muon_pt[Muon_charge>0][0]")
     .Define("Mu1_hasTriggerMatch", hasTriggerMatch, {"Mu1_eta", "Mu1_phi", "goodTrigObjs_eta", "goodTrigObjs_phi"})
-    .Define("Mu2_eta", "Muon_eta[prefIdx[1]]")
-    .Define("Mu2_phi", "Muon_phi[prefIdx[1]]")
-    .Define("Mu2_charge", "Muon_charge[prefIdx[1]]")
-    .Define("Mu2_relIso", "Muon_pfRelIso04_all[prefIdx[1]]")
-    .Define("Mu2_pt", "Muon_pt[prefIdx[1]]")
+    .Define("Mu2_eta", "Muon_eta[Muon_charge<0][0]")
+    .Define("Mu2_phi", "Muon_phi[Muon_charge<0][0]")
+    .Define("Mu2_charge", "Muon_charge[Muon_charge<0][0]")
+    .Define("Mu2_relIso", "Muon_pfRelIso04_all[Muon_charge<0][0]")
+    .Define("Mu2_pt", "Muon_pt[Muon_charge<0][0]")
     .Define("dimuonP4", getdimuonP4, {"Mu1_pt", "Mu1_eta", "Mu1_phi", "Mu2_pt", "Mu2_eta", "Mu2_phi"})
     .Define("dimuonMass", [this](TLorentzVector p){ return float(p.M());}, {"dimuonP4"})
     .Define("dimuonPt", [this](TLorentzVector p){ return float(p.Pt());}, {"dimuonP4"})
