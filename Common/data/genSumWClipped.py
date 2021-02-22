05b873e0b0150826bfa5f93b963c8138168b02a0
Suvankar Roy Chowdhury <suvankar.roy.chowdhury@cern.ch>
2021-02-22 13:58:19 +0100

    updating genEvsum and SF file

--- Common/data/genSumWClipped.py
+++ Common/data/genSumWClipped.py
@@ -1,28 +1,8 @@
-#using genWeight as the branch
-sumwClippedDict = {
-    "DYJetsToMuMu_M50": 235996573447.5537,
-    "DYJetsToTauTau_M50": 117740866311.92139,
-    "WPlusJetsToMuNu": 5895489808249.853,
-    "WMinusJetsToMuNu": 2001402133898.8691,
-    "WPlusJetsToTauNu": 805332767237.3389,
-    "WMinusJetsToTauNu": 357281504749.76074
+sumwClippedDict= {
+    "DYJetsToMuMu_M50": 235996656029.08496,
+    "DYJetsToTauTau_M50": 117740897803.58936,
+    "WPlusJetsToMuNu": 5895502305412.54,
+    "WMinusJetsToMuNu": 2001405081724.4512,
+    "WPlusJetsToTauNu": 805334479658.9951,
+    "WMinusJetsToTauNu": 357282055597.0303
 }
-
-'''
-#using Generator weight
-sumwClippedDict = {
-    "DYJetsToMuMu_M50":  236002045557.625, #Marco 2.36619888176e+11,  #236002045557.625, 
-    "DYJetsToTauTau_M50": 117743596494.25, #Marco 1.17935409707e+11, #117743596494.25, 
-    "WPlusJetsToMuNu": 5895447715506.5, 
-    "WMinusJetsToMuNu": 2001441443604.5, 
-    "WPlusJetsToTauNu": 805327017069.5, 
-    "WMinusJetsToTauNu": 357288521895.0, 
-    "WZ": 680128.0, 
-    "WW": 1843183.250793457, 
-    "ST_t-channel_top_5f_InclusiveDecays": 394691602.59375, 
-    "ST_t-channel_tauDecays": 12390.0, 
-    "ST_t-channel_muDecays": 254958.0, 
-    "ST_s-channel_4f_leptonDecays": 3687399.50390625
-}
-'''
-
